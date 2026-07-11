42e9b68082344ba85f50039ade908f4e8b868c61
palpalcode <palpalcode@gmail.com>
2026-07-11 20:08:31 +0700

    Route API catch-all on Vercel

--- api/[...path].js
+++ api/[...path].js
@@ -1,3 +1,24 @@
-import handler from "../artifacts/api-server/dist/vercel.mjs";
+let backendHandlerPromise;
 
-export default handler;
+function restoreOriginalApiUrl(req) {
+  const currentUrl = new URL(req.url || "/", "http://vercel.local");
+  const apiPath = currentUrl.searchParams.get("__path");
+
+  if (apiPath === null) {
+    return;
+  }
+
+  currentUrl.searchParams.delete("__path");
+  const query = currentUrl.searchParams.toString();
+  req.url = `/api/${apiPath}${query ? `?${query}` : ""}`;
+}
+
+export default async function handler(req, res) {
+  backendHandlerPromise ??= import("../artifacts/api-server/dist/vercel.mjs").then(
+    (mod) => mod.default,
+  );
+
+  restoreOriginalApiUrl(req);
+  const backendHandler = await backendHandlerPromise;
+  return backendHandler(req, res);
+}
